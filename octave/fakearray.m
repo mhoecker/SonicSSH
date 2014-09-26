efc1f5d6bbad7a4486fec7a1e6717d059565a5aa
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2014-09-25 17:28:11 -0700

    add multiple wavelengths and amplitudes

--- octave/fakearray.m
+++ octave/fakearray.m
@@ -3,32 +3,37 @@ R = 1;
 # Array size
 Order = 3;
 # Record Length (sec)
-T = 60;
-# Wavelength (m)
-L = 100*R;
+T = 240;
+# Wavelengths (m)
+L = R*(55+45*rand(1,4));
+# Amplitude
+A = rand(size(L));
 # Orientation (degrees)
 #theta = pi*rand(1,1);
-theta = pi/2;
+theta = pi*rand(size(L));
 # create an array
 x = [];
 for i=1:Order
  for j=1:i
    x = [x;j-(i+1)/2,(i-1)*sin(pi/3)];
- end%for
-end%for
+ end#for
+end#for
 x = R*x;
 Nx = length(x(:,1));
 x0 = mean(x,1);
 # Assume 10Hz sample rate
 t = [0:.1:T];
-# Wave charachteristics
-k = [cos(theta),sin(theta)]*2*pi/L;
-omega = sqrt(10*sqrt(sum(k.^2)));
-#
-# Wave height
-h = sin(x*k'.-omega*t);
+Nt = length(t);
 # Add noise
-h = h+rand(size(h))/20;
+h = rand(Nx,Nt)/50;
+#Add all the waves
+for i=1:length(L)
+# Wave charachteristics
+ k = [cos(theta(i)),sin(theta(i))]*2*pi/L(i);
+ omega = sqrt(10*sqrt(sum(k.^2)));
+# Wave height
+ h = h+A(i).*sin(x*k'.-omega*t);
+end#for
 #
 # matrix of diferences
 # A =
@@ -42,53 +47,58 @@ h = h+rand(size(h))/20;
 #     .   ,.   ,.   ,.
 #     .   ,.   ,.   ,.
 #
-A = ones(Nx,Nx);
-A(1,:) = 1;
+Taylor = ones(Nx,Nx);
+Taylor(1,:) = 1;
 # dx,dy
 if(Nx>2)
- A(2,:) = x(:,1)-x0(1);
- A(3,:) = x(:,2)-x0(2);
-end%if
+ Taylor(2,:) = x(:,1)-x0(1);
+ Taylor(3,:) = x(:,2)-x0(2);
+end#if
 # dx^2, dx*dy, dy^2
 if(Nx>5)
- A(4,:) = .5*(x(:,1)-x0(1)).^2;
- A(5,:) = .5*(x(:,2)-x0(2)).^2;
- A(6,:) = (x(:,2)-x0(2)).*(x(:,1)-x0(1));
-end%if
+ Taylor(4,:) = .5*(x(:,1)-x0(1)).^2;
+ Taylor(5,:) = .5*(x(:,2)-x0(2)).^2;
+ Taylor(6,:) = (x(:,2)-x0(2)).*(x(:,1)-x0(1));
+end#if
 #
 # oops that should be transpossed
-A = A';
+Taylor = Taylor';
 # Taylor expansion looks like
 #
-#       mean(h)
-#       dh/dx
-#       dh/dy
-# A  *  d^2h/dx^2 = h
-#       d^2h/dy^2
-#       d^2h/dxdy
+#         mean(h)
+#         dh/dx
+#         dh/dy
+#Taylor * d^2h/dx^2 = h
+#         d^2h/dy^2
+#         d^2h/dxdy
 #         .
 #         .
 #         .
 #
 #
 # Invert the matrix
-Ainv = inv(A);
+Taylorinv = inv(Taylor);
 #
 # Calculate derrivatives
-hders = Ainv*h;
+hders = Taylorinv*h;
 #
 #
 figure(1)
-plot(t,h-hders(1,:))
-figure(2)
 subplot(2,1,1)
 plot(t,atan2(hders(3,:),hders(2,:))*180/pi)
 axis([0,T,-180,180])
 xlabel("time (sec)")
 ylabel("Wave face direction")
-subplot(2,1,2)
-plot(hders(2,:),hders(3,:),"o;;");
-axis(4*pi*[-1,1,-1,1]/L);
+subplot(2,2,3)
+plot(hders(2,:),hders(3,:),".;;");
+axis(length(L)*pi*[-1,1,0,1]/min(L));
+axis equal;
+xlabel("A*k_x");
+ylabel("A*k_y");
+drawnow
+subplot(2,2,4)
+plot(+A.*2.*pi.*cos(theta)./L,+A.*2.*pi.*sin(theta)./L,"+;+wave vectors;",-A.*2.*pi.*cos(theta)./L,-A.*2.*pi.*sin(theta)./L,".;-wave vectors;");
+axis(2*pi*[-1,1,0,1]/min(L));
 axis equal;
 xlabel("A*k_x");
 ylabel("A*k_y");
